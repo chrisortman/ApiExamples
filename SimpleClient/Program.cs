ceb15b8a69b67b66c54059c86a56929ae22903f6
Chris Ortman <chrisortman@gmail.com>
2012-12-20 09:02:42 -0600

    Add filter examples

--- SimpleClient/Program.cs
+++ SimpleClient/Program.cs
@@ -28,56 +28,114 @@ namespace SimpleClient
             else
             {
                 sericeClient = new JsonServiceClient("http://localhost:32284");
+
             }
 
-            Console.WriteLine("Example Successful Login:");
-            var request = new Login() {Username = "chris", Password = "sirhc"};
-            var user = sericeClient.Post(request);
-            user.PrintDump();
+            EnableRequestTiming((JsonServiceClient) sericeClient);
 
-            try {
-                Console.WriteLine("Example Invalid Login");
-                request = new Login() {Username = "chris", Password = "xxx"};
-                user = sericeClient.Post(request);
-            }
-            catch (WebServiceException webEx) {
-                Console.WriteLine("Caught Exception:");
-                Console.WriteLine(webEx.ToString());
-            }
+            SuccessfulLogin(sericeClient);
 
-            Console.WriteLine("Login successfully using Login2:");
-            var response = sericeClient.Post<Login2Response>("/security/login2",new Login2 {Username = "chris",Password = "sirhc"});
-            response.PrintDump();
+            //InvalidLogin(sericeClient);
 
-            Console.WriteLine("Bad login with HttpError return:");
-            try {
-                Console.WriteLine("Example Invalid Login");
-                response = sericeClient.Post<Login2Response>("/security/login2",new Login2 {Username = "chris",Password = "xxx"});
-            }
-            catch (WebServiceException webEx) {
-                Console.WriteLine("Caught Exception:");
-                Console.WriteLine(webEx.ToString());
-            }
+            //SuccessfulLogin2ShowsAlternativeReturnFormat(sericeClient);
 
-             Console.WriteLine("Bad login with thrown exception:");
-            try {
-                Console.WriteLine("Example Invalid Login");
-                response = sericeClient.Post<Login2Response>("/security/login2",new Login2 {Username = "Bchris",Password = "xxx"});
-            }
-            catch (WebServiceException webEx) {
-                Console.WriteLine("Caught Exception:");
-                Console.WriteLine(webEx.ToString());
-            }
+            //BadLoginWithHttpErrorReturn(sericeClient);
 
-          
+            //BadLoginWithThrownException(sericeClient);
+
+
+            Finish(inProcHost);
+        }
+
+        private static void EnableRequestTiming(JsonServiceClient sericeClient)
+        {
+            DateTime start = DateTime.MinValue;
+            string requestUrl = "";
+            sericeClient.LocalHttpWebRequestFilter = request =>
+            {
+                start = DateTime.UtcNow;
+                requestUrl = request.RequestUri.ToString();
+            };
+
+            sericeClient.LocalHttpWebResponseFilter = response =>
+            {
+                Console.WriteLine("Web Service Call to " + requestUrl + " took: " + DateTime.UtcNow.Subtract(start));
+            };
+        }
+
+        private static void Finish(InProcessAppHost inProcHost)
+        {
             Console.WriteLine("Press [enter] to exit");
             Console.ReadLine();
-            
+
             if (inProcHost != null)
             {
                 inProcHost.Stop();
                 inProcHost.Dispose();
             }
+        }
+
+        private static void BadLoginWithThrownException(IRestClient sericeClient)
+        {
+            Console.WriteLine("Bad login with thrown exception:");
+            try
+            {
+                Console.WriteLine("Example Invalid Login");
+                var response = sericeClient.Post<Login2Response>("/security/login2",
+                                                             new Login2 {Username = "Bchris", Password = "xxx"});
+            }
+            catch (WebServiceException webEx)
+            {
+                Console.WriteLine("Caught Exception:");
+                Console.WriteLine(webEx.ToString());
+            }
+        }
+
+        private static void BadLoginWithHttpErrorReturn(IRestClient sericeClient)
+        {
+            Console.WriteLine("Bad login with HttpError return:");
+            try
+            {
+                Console.WriteLine("Example Invalid Login");
+                var response = sericeClient.Post<Login2Response>("/security/login2",
+                                                             new Login2 {Username = "chris", Password = "xxx"});
+            }
+            catch (WebServiceException webEx)
+            {
+                Console.WriteLine("Caught Exception:");
+                Console.WriteLine(webEx.ToString());
+            }
+        }
+
+        private static void SuccessfulLogin2ShowsAlternativeReturnFormat(IRestClient sericeClient)
+        {
+            Console.WriteLine("Login successfully using Login2:");
+            var response = sericeClient.Post<Login2Response>("/security/login2",
+                                                             new Login2 {Username = "chris", Password = "sirhc"});
+            response.PrintDump();
+        }
+
+        private static void InvalidLogin(IRestClient sericeClient)
+        {
+            try
+            {
+                Console.WriteLine("Example Invalid Login");
+                var request = new Login() {Username = "chris", Password = "xxx"};
+                var user = sericeClient.Post<SecurityUser>(request);
+            }
+            catch (WebServiceException webEx)
+            {
+                Console.WriteLine("Caught Exception:");
+                Console.WriteLine(webEx.ToString());
+            }
+        }
+
+        private static void SuccessfulLogin(IRestClient sericeClient)
+        {
+            Console.WriteLine("Example Successful Login:");
+            var request = new Login() {Username = "chris", Password = "sirhc"};
+            var user = sericeClient.Post(request);
+            user.PrintDump();
         }
     }
 
